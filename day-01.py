d5200351339ee18f74f3a2684b4d61d11a4705c9
Kenneth Collins <dev@kecors.net>
2022-08-16 17:22:53 -0400

    Solve day 1, part 2

--- day-01.py
+++ day-01.py
@@ -3,15 +3,19 @@
 def parse_input(filename):
     return open(filename).read().strip()
 
+def follow_instruction(floor, instruction):
+    if instruction == '(':
+        floor += 1
+    elif instruction == ')':
+        floor -= 1
+    else:
+        print("Unknown instruction " + instruction)
+    return floor
+
 def follow_all_instructions(instructions):
     floor = 0
     for instruction in instructions:
-        if instruction == '(':
-            floor += 1
-        elif instruction == ')':
-            floor -= 1
-        else:
-            print("Unknown instruction " + instruction)
+        floor = follow_instruction(floor, instruction)
     return floor
 
 def test_follow_all_instructions():
@@ -25,6 +29,20 @@ def test_follow_all_instructions():
     assert follow_all_instructions(')))') == -3
     assert follow_all_instructions(')())())') == -3
 
+def find_position_at_basement_entry(instructions):
+    floor = 0
+    position = 1
+    for instruction in instructions:
+        floor = follow_instruction(floor, instruction)
+        if floor == -1:
+            return position
+        else:
+            position += 1
+
+def test_find_position_at_basement_entry():
+    assert find_position_at_basement_entry(')') == 1
+    assert find_position_at_basement_entry('()())') == 5
+
 ####################
 
 instructions = parse_input('puzzle-input.txt')
@@ -33,3 +51,8 @@ instructions = parse_input('puzzle-input.txt')
 test_follow_all_instructions()
 floor = follow_all_instructions(instructions)
 print("Part 1: the instructions take Santa to floor " + str(floor))
+
+# Part 2
+test_find_position_at_basement_entry()
+position = find_position_at_basement_entry(instructions)
+print("Part 2: the position where Santa enters the basement is " + str(position))
